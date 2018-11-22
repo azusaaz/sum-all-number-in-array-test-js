c783fb1b84b57090c320da0026e69b3d241f107b
“az” <shimazaki@aaa.email.ne.jp>
2018-11-21 22:37:04 -0500

    add sumItems function

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let result = 0;
+
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      result += sumItems(item);
+
+    } else {
+      result += item;
+
+    }
+  });
+  
+  return result;
 }
 
 module.exports = sumItems;
